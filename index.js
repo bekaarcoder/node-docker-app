e53d89e597d034b44e1ca33e20e5bb4e1a0988ca
Shashank <s_shashank@outlook.com>
2021-05-03 19:57:06 +0530

    communicate between node and mongodb containers

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+    MONGO_USER,
+    MONGO_PASSWORD,
+    MONGO_IP,
+    MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+mongoose
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log("Database connected.");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 
 app.get("/", (req, res) => {
     res.send("<h2>Hello World from Docker Container!</h2>");
